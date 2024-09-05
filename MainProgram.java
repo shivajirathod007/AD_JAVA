e7915cdc47d8646dbfca8e8e5c050da202dee4de
shivajirathod007 <shivajira222@gmail.com>
2024-09-05 19:39:26 +0530

    added a new button

--- MainProgram.java
+++ MainProgram.java
@@ -1,15 +1,15 @@
 import javax.swing.*;
 
-public class MainProgram extends JFrame{
-    MainProgram(){
+public class MainProgram extends JFrame {
+    MainProgram() {
         setVisible(true);
-        setSize(500,500);
+        setSize(500, 500);
         setTitle("Ticket Booking System");
-
+        JButton jb = new JButton("trial");
+        add(jb);
     }
 
-
-    public static void main(String arg[]){
+    public static void main(String arg[]) {
         MainProgram mp = new MainProgram();
     }
 }
