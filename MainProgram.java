4e45f7ef0eb8e4126d5223992bbc95da2c8f6579
Shivaji Mallappa Rathod <122688694+shivajirathod007@users.noreply.github.com>
2024-09-05 13:43:32 +0000

    initial Commet

---
+++ MainProgram.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class MainProgram extends JFrame{
+    MainProgram(){
+        setVisible(true);
+        setSize(500,500);
+        setTitle("Ticket Booking System");
+
+    }
+
+
+    public static void main(String arg[]){
+        MainProgram mp = new MainProgram();
+    }
+}
